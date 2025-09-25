bc321e7f7a2ab509936a3433afcc9a7e2afbc637
jahjah103104 <jahlilaamoncio01@gmail.com>
2025-09-25 17:53:39 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import CommentSection from './CommentSection';
+import ChatMessage from './ChatMessage';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <ChatMessage/>
+      <CommentSection/>
+    </>
+  )
+    }
